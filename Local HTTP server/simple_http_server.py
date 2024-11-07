50d42a3a7fd676e04c1a3eb8f34d3634eaadb6af
oinsio <oinsio.srv@gmail.com>
2024-11-08 00:57:15 +0500

    Add Register account API to the simple HTTP server

--- Local HTTP server/simple_http_server.py
+++ Local HTTP server/simple_http_server.py
@@ -1,10 +1,14 @@
 import json
 import logging
+import uuid
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 # Configure logger
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 logger = logging.getLogger()
+
+# Registered email <-> userId storage
+registered_accounts = dict()
 
 class SimpleAPIHandler(BaseHTTPRequestHandler):
     def log_request_id(self, request_id):
@@ -20,22 +24,15 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
         self.log_request_id(request_id)
 
         if self.path == "/api/v1/hello":
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.send_header("X-Request-ID", request_id)
-            self.end_headers()
-
             response = {
                 "success": True,
                 "data": {
                     "message": "Hello, World!"
                 }
             }
-            self.wfile.write(json.dumps(response).encode('utf-8'))
+            self.send_status_headers_response(200, request_id, response)
         else:
-            self.send_response(404)
-            self.send_header("X-Request-ID", request_id)
-            self.end_headers()
+            self.send_not_found(request_id)
 
     def do_POST(self):
         """POST-request handler"""
@@ -43,46 +40,90 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
         self.log_request_id(request_id)
 
         if self.path == "/api/v1/greet":
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length)
+            self.handle_greet_request(request_id)
+        elif self.path == "/api/v1/register":
+            self.handle_register_request(request_id)
+        else:
+            self.send_not_found(request_id)
 
-            try:
-                data = json.loads(post_data.decode('utf-8'))
-                name = data.get("name", "stranger")
+    def handle_register_request(self, request_id):
+        """Register request handler"""
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
 
-                self.send_response(200)
-                self.send_header("Content-type", "application/json")
-                self.send_header("X-Request-ID", request_id)
-                self.end_headers()
+        try:
+            data = json.loads(post_data.decode('utf-8'))
+            email = data.get("email", "")
+
+            if not email.strip():
+                response = {
+                    "success": False,
+                    "data": {
+                        "error": "Empty email"
+                    }
+                }
+                self.send_status_headers_response(400, request_id, response)
+            else:
+                userId = str(uuid.uuid4())
+                registered_accounts[email] = userId
 
                 response = {
                     "success": True,
                     "data": {
-                        "message": f"Hello, {name}!"
+                        "userId": userId,
+                        "message": f"Account {email} successfully registered."
                     }
                 }
-                self.wfile.write(json.dumps(response).encode('utf-8'))
-            except json.JSONDecodeError:
-                response = {
-                    "success": False,
-                    "data": {
-                        "error": "Invalid JSON"
-                    }
+                self.send_status_headers_response(200, request_id, response)
+        except json.JSONDecodeError:
+            self.send_invalid_json_response(request_id)
+        logger.info(f"Registered accounts: {registered_accounts}")
+
+    def handle_greet_request(self, request_id):
+        """Greeting request handler"""
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+
+        try:
+            data = json.loads(post_data.decode('utf-8'))
+            name = data.get("name", "stranger")
+
+            response = {
+                "success": True,
+                "data": {
+                    "message": f"Hello, {name}!"
                 }
-                self.send_response(400)
-                self.send_header("X-Request-ID", request_id)
-                self.end_headers()
-                self.wfile.write(json.dumps(response).encode('utf-8'))
-        else:
-            self.send_response(404)
-            self.send_header("X-Request-ID", request_id)
-            self.end_headers()
+            }
+            self.send_status_headers_response(200, request_id, response)
+        except json.JSONDecodeError:
+            self.send_invalid_json_response(request_id)
+
+    def send_not_found(self, request_id):
+        self.send_response(404)
+        self.send_header("X-Request-ID", request_id)
+        self.end_headers()
+
+    def send_status_headers_response(self, status, request_id, response):
+        self.send_response(status)
+        self.send_header("X-Request-ID", request_id)
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+
+    def send_invalid_json_response(self, request_id):
+        response = {
+            "success": False,
+            "data": {
+                "error": "Invalid JSON"
+            }
+        }
+        self.send_status_headers_response(400, request_id, response)
 
 def run(server_class=HTTPServer, handler_class=SimpleAPIHandler, port=3000):
     """Launch server on 3000 port"""
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print(f"Starting server on port {port}...")
+    logger.info(f"Registered accounts: {registered_accounts}")
     httpd.serve_forever()
 
 if __name__ == '__main__':
